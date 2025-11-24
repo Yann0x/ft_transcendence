7598d3088d89e08ac263bb097c162621ba27cfce
1Batmain <batduval@gmail.com>
2025-11-24 15:25:12 +0100

    clean

--- src/plugins/static.ts
+++
@@ -1,9 +0,0 @@
-import fastifyStatic from "@fastify/static";
-import path from "path";
-
-export default async function (app: any) {
-  app.register(fastifyStatic, {
-    root: path.join(__dirname, "..", "..", "public"),
-  });
-}
-
