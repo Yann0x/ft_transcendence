775572d02d0cdf972673c27f562d77f87f3cc731
1Batmain <batduval@gmail.com>
2025-11-27 12:01:30 +0100

    le proxy sert le front comme il se doit

---
+++ srcs/frontend/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+echo -e $DISP_BOLD $DISP_ORANGE "Installing dependencies..." $DISP_DFLT
+npm install
+
+echo $DISP_BOLD $DISP_ORANGE "Starting development server..." $DISP_DFLT
+exec npm run dev
