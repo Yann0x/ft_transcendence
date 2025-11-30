796774df4700b1bfe7ed09fc740ff04bf28c5964
1Batmain <batduval@gmail.com>
2025-11-30 13:32:27 +0100

    Shared folder partagé entre tous les containers

---
+++ srcs/backend/shared/types/game.ts
@@ -0,0 +1,32 @@
+import { User } from "./user";
+// example a addapter 
+
+export interface Game {
+    id: number;
+    player1Id: User;
+    player2Id: User;
+    player1Score: number;
+    player2Score: number;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface Ball {
+    x: number;
+    y: number;
+    radius: number;
+    velocityX: number;
+    velocityY: number;
+}
+
+export interface Paddle {
+    y: number;
+    width: number;
+    speed: number;
+}
+
+export interface GameState {
+    ball: Ball;
+    paddle1: Paddle;
+    paddle2: Paddle;
+}
