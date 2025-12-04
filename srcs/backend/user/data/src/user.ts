a33ebe67fce223ec5b850c52cc8ad1d73932062f
1Batmain <batduval@gmail.com>
2025-12-04 15:11:29 +0100

    user wip

--- srcs/backend/user/data/src/user.ts
+++ srcs/backend/user/data/src/user.ts
@@ -4,6 +4,34 @@ import { UserQuery, UserQueryResponse, UserRegister, UserUpdate } from './shared
 const server = fastify()
 
 
+server.post<{ Body: UserRegister }>('/user', async (request, reply) => {
+  // TODO email unique
+  // TODO hash password
+  // TODO store user in database
+  // TODO créer JWT
+  // TODO return JWT
+})
+
+server.put<{ Body: UserUpdate, Response: {success: boolean}}>('/user', async (request, reply) => {
+  // TODO vérifier JWT
+  // TODO update user in database
+  // TODO return success status
+})
+
+server.delete<{ Body: UserQuery, Response: {success: boolean}}>('/user', async (request, reply) => {
+  // TODO vérifier JWT
+  // TODO delete user from database
+  // TODO return success status
+})
+
+server.get<{ Body: UserQuery, Response: UserQueryResponse }>('/user', async (request, reply) => {
+  // TODO vérifier JWT
+  // TODO get user from database
+    // Si valid JWT 
+      // Si admin OU meme user
+        //  return UserQuery 
+    // Sinon return UserPublic
+})
 
 server.listen({ port: 3000, host: '0.0.0.0'}, (err, address) => {
   if (err) {
