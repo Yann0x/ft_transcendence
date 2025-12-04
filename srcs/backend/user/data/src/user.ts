669fd260a2ed5b52119d57fb9264085772a63ace
1Batmain <batduval@gmail.com>
2025-12-04 08:53:42 +0100

    base propre

--- srcs/backend/user/data/src/user.ts
+++ srcs/backend/user/data/src/user.ts
@@ -1,11 +1,6 @@
 import fastify from 'fastify'
-import { Register } from './shared/types/user'
 
 const server = fastify()
-
-server.post<{Body: Register}> ('/register', async (request, reply) => {
-
-})
 
 server.listen({ port: 3000, host: '0.0.0.0'}, (err, address) => {
   if (err) {
