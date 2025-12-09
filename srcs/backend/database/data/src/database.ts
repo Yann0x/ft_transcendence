5f40d1908dcc005dbc60ff9846e5feac71ac552b
1Batmain <batduval@gmail.com>
2025-12-09 13:59:40 +0100

    meilleur approche avec les schemas

--- srcs/backend/database/data/src/database.ts
+++ srcs/backend/database/data/src/database.ts
@@ -7,55 +7,14 @@ const server = fastify()
 
 db.initializeDatabase();
 
-async function logRequest(request: fastify.FastifyRequest, reply: fastify.FastifyReply) {
+async function preHandler(request: fastify.FastifyRequest, reply: fastify.FastifyReply) {
   console.log(`Received ${request.method} request for ${request.url}`);
 }
 
-server.addHook('onRequest', logRequest);
+server.addHook('onRequest', preHandler);
 
 /* GESTION USER */
 
-  server.get<{Querystring: UserQuery, Response: UserQueryResponse[]}>('/user', async (request, reply) => {
-    const users: UserQueryResponse[] = db.getUser(request.query);
-    if (users.length === 0) {
-      reply.status(404).send({ error: 'User not found' });
-      return;
-    }
-    return users;
-  })
-
-  server.put<{Body: UserUpdate, Response: boolean}>('/user', async (request, reply) => {
-    const result = db.updateUser(request.body);
-    if (!result) {
-      reply.status(404).send({ error: 'User not found or no changes made' });
-      return;
-    }
-    return result;
-  })
-
-  server.post<{Body: UserRegister, Response: boolean}>('/user', async (request, reply) => {
-    console.log('Registering user: ', request.body);
-    const result = db.createUser(request.body);
-    if (!result) {
-      reply.status(500).send({ error: 'User could not be created' });
-      return;
-    }
-    return result;
-  })
-
-  server.delete<{Body: UserQuery, Response: boolean}>('/user', async (request, reply) => {
-    const result = db.deleteUser(request.body);
-    if (!result) {
-      reply.status(404).send({ error: 'User not found' });
-      return;
-    }
-    return result;
-  })
-
-  server.get<{Body: UserQuery, Response : string}>('/user/password_hash', async (request, reply) => {
-    const password_hash = db.getUserPasswordHash(request.body);
-    return password_hash;
-  })
 
 
 server.listen({ port: 3000, host: '0.0.0.0'}, (err, address) => {
