2213943d9092e21e376ced1baaf374c796bb4ced
1Batmain <batduval@gmail.com>
2025-12-12 09:43:54 +0100

    enable https

--- srcs/backend/proxy/data/src/proxy.ts
+++ srcs/backend/proxy/data/src/proxy.ts
@@ -1,9 +1,26 @@
 import fastify, { FastifyRequest, FastifyReply } from 'fastify'
 import fastifyStatic from '@fastify/static'
 import proxy from '@fastify/http-proxy'
+import fs from 'fs'
+import path from 'path'
 import {SenderIdentity} from './shared/types/user'
 
-const server = fastify({ logger: true })
+// self signed certificate 
+const keyPath = '/certs/selfsigned.key'
+const certPath = '/certs/selfsigned.crt'
+
+if (!fs.existsSync(keyPath) || !fs.existsSync(certPath)) {
+  console.error('[PROXY] TLS key/cert not found. Generate with openssl and mount to /certs.');
+  process.exit(1)
+}
+
+const server = fastify({
+  logger: true,
+  https: {
+    key: fs.readFileSync(path.resolve(keyPath)),
+    cert: fs.readFileSync(path.resolve(certPath)),
+  }
+})
 
 // Log incoming requests
 server.addHook('onRequest', async (request, reply) => {
--- srcs/backend/proxy/entrypoint.sh
+++ srcs/backend/proxy/entrypoint.sh
@@ -7,5 +7,16 @@ cd /data
 echo  $DISP_BOLD $DISP_ORANGE "Installing packages dependencies" $DISP_DFLT 
 npm i
 
+if [ -d /certs/ && -f /certs/selfsigned.crt ]; then
+    echo  $DISP_BOLD $DISP_ORANGE "Detecte Certificate" $DISP_DFLT 
+else 
+    echo  $DISP_BOLD $DISP_ORANGE "Generating self-signed certificate for https" $DISP_DFLT 
+    mkdir -p /certs
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+        -keyout /certs/selfsigned.key \
+        -out /certs/selfsigned.crt \
+        -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.example.com"
+fi
+
 echo  $DISP_BOLD $DISP_ORANGE "Running server" $DISP_DFLT 
 exec $@
